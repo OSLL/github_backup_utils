24655d72f20303c9c1e2698e8cd008ce577fe776
Yaroslav Gosudarkin <gosudarkin.yaroslav@gmail.com>
2022-03-03 20:50:06 +0300

    clone repos

--- clone_repos.sh
+++ clone_repos.sh
@@ -7,12 +7,23 @@ INPUT=${1:-"file.csv"}
 ORG=${2:-"org"}
 OLDIFS=$IFS
 IFS=';'
+
+if [ ! -d "$ORG" ]; then
+  mkdir $ORG
+fi
+
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read name priv issues perms
 do
 	LINK="git@github.com:$ORG/$name.git"
-	echo "Clone Repo : $LINK"
-	git clone $LINK
+    if [ ! -d "$name" ]; then
+	    git clone $LINK
+    else
+      echo "REPO $name EXISTS. FETCHING."
+      cd $name
+      git fetch
+      cd ..
+    fi
 	echo "__________________________________________"
 done < $INPUT
 IFS=$OLDIFS

ad44487bb10aebdf1faf00d0a0c1cf759126e7de
Yaroslav Gosudarkin <gosudarkin.yaroslav@gmail.com>
2022-03-03 18:13:37 +0300

    added script for cloning repos

--- clone_repos.sh
+++ clone_repos.sh
@@ -8,7 +8,7 @@ ORG=${2:-"org"}
 OLDIFS=$IFS
 IFS=';'
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read name priv ssn tel status
+while read name priv issues perms
 do
 	LINK="git@github.com:$ORG/$name.git"
 	echo "Clone Repo : $LINK"

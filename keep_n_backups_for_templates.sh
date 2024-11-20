1d44913db6f0e5c2adf1d49d2882cda94f9309cf
KonstantinKondratenko <sider123456789101112131415@gmail.com>
2024-11-20 16:54:28 +0300

    add prefix

--- keep_n_backups_for_templates.sh
+++ keep_n_backups_for_templates.sh
@@ -8,21 +8,23 @@ if [ ! -d "$BACKUP_DIR" ]; then
   echo "Folder $BACKUP_DIR created"
 fi
 
-if [ "$#" -lt 2 ]; then
-  echo "Usage: $0 N folder1 [folder2 ... folderN]"
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 N prefix"
   exit 1
 fi
 
 N=$1
-shift
-FOLDERS=("$@")
+PREFIX=$2
 
+FOLDERS=($(find . -maxdepth 1 -type d -name "${PREFIX}*" -printf "%f\n"))
+
+# echo "Finded foldors: ${FOLDERS[@]}"  # дебаг вывод чтобы посмотреть на то, какие папки нашлись по префиксу
 
 copy_folder() {
   local folder=$1
   local timestamp=$(date +"%Y_%m_%d_%H_%M")
   local new_folder_name="${folder}_${timestamp}"
-  echo "Star copy $folder into $BACKUP_DIR/$new_folder_name"
+  echo "Start copy $folder into $BACKUP_DIR/$new_folder_name"
   cp -r -f "$folder" "$BACKUP_DIR/$new_folder_name"
   echo "Folder $folder copied into $BACKUP_DIR/$new_folder_name"
 }
@@ -39,7 +41,7 @@ clean_old_copies() {
   local folder=$1
   local pattern="${folder}_*"
   local copies=("$BACKUP_DIR"/$pattern)
-  # echo ${copies[@]}   # дебаг вывод чтобы посмотреть на то, как исщутся копии 
+  # echo ${copies[@]}   # дебаг вывод чтобы посмотреть на то, как ищутся копии
 
   if [ ${#copies[@]} -gt $N ]; then
     local num_to_delete=$(( ${#copies[@]} - N ))
